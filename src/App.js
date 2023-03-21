ac0d8bdd32e0904814552ad280787458bb41f438
farhan397 <farhanhamdani397@gmail.com>
2023-03-20 17:26:44 -0700

    comitnew

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState,useEffect} from 'react'
-
-import Imgcard from './Imgcard/Imgcard';
-import Datafilter from './DataFilter/Datafilter';
-
-
+import Foam1 from './component/Foam1';
 
 function App() {
-  const[datesss,setdates]=useState("");
-const[roverss,setroverss]=useState("");
-const[cam,setcam]=useState("");
-const[btnsearch1,setbtnsearch1]=useState(0);
-
-  
-  const btnsearch=(rovers,camera,datess)=>{
-    
-   
-    setdates(datess);
-    setroverss(rovers);
-    setcam(rovers);
-    setbtnsearch1(1)
- }
-
- 
-  
   return (
     <div className="App">
-      {/* <div>
-        <Datafilter btnsearch={btnsearch}/>
-      </div> */}
-      <div>
-        <Imgcard />
-        
-      </div>
+      <Foam1/>
     </div>
   );
 }
